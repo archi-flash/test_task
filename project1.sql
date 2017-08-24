edbff89afc3caa7f22ee06e1b750802ba5275a62
Arkady <arkady.hramov@ihousedesign.com>
2017-08-24 15:44:50 +0700

    dump

---
+++ project1.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.3
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost:3306
+-- Время создания: Авг 24 2017 г., 01:42
+-- Версия сервера: 5.6.31
+-- Версия PHP: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `project1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(10) NOT NULL,
+  `email` char(50) NOT NULL,
+  `password` char(60) NOT NULL,
+  `last_login_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `password`, `last_login_time`) VALUES
+(6, 'archi.khramov@gmail.com', '$2y$10$h7EwvjDau.aU81AaGR1MLO4RheIokArwvB51vRIn6O8T0nFghG3oq', '2017-08-24 07:38:58');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users_data`
+--
+
+CREATE TABLE `users_data` (
+  `id` int(10) NOT NULL COMMENT 'users.id',
+  `fullname` char(64) NOT NULL DEFAULT '',
+  `picture` char(100) NOT NULL DEFAULT ''
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users_data`
+--
+
+INSERT INTO `users_data` (`id`, `fullname`, `picture`) VALUES
+(6, 'Sebastien Boin', '1503560338.jpg');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email_key` (`email`);
+
+--
+-- Индексы таблицы `users_data`
+--
+ALTER TABLE `users_data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
